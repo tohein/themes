cc768f0f4e0171d87e6bdf12be0fd3a2f2f1c71c
Tobi Heinen <t.heinen@dkfz.de>
2023-05-02 10:38:09 +0200

    Added Kanagawa

---
+++ themes/Kanagawa Normal.js
@@ -0,0 +1,4 @@
+t.prefs_.set("color-palette-overrides",["#16161d", "#c34043", "#76946a", "#c0a36e", "#7e9cd8", "#957fb8", "#6a9589", "#c8c093", "#727169", "#e82424", "#98bb6c", "#e6c384", "#7fb4ca", "#938aa9", "#7aa89f", "#dcd7ba"]);
+t.prefs_.set("foreground-color", "#dcd7ba");
+t.prefs_.set("background-color", "#1f1f28");
+t.prefs_.set("cursor-color", "rgba(200, 192, 147, 1)");
